b336e0039b8328bda008d2ff03dd69df46fb4cb0
Igor <igor@devfire.io>
2023-11-05 13:47:17 -0500

    added initial protocol.rs

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,28 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-
-use std::env;
-use std::error::Error;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    // Allow passing an address to listen on as the first argument of this
-    // program, but otherwise we'll just set up our TCP listener on
-    // 127.0.0.1:8080 for connections.
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:6379".to_string());
-
-    // Next up we create a TCP listener which will listen for incoming
-    // connections. This TCP listener is bound to the address we determined
-    // above and must be associated with an event loop.
-    let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on: {}", addr);
-
-    loop {
-        // Asynchronously wait for an inbound socket.
-        let (mut socket, _) = listener.accept().await?;
-
-        // And this is where much of the magic of this server happens. We
-        // crucially want all clients to make progress concurrently, rather than
-        // blocking one on completion of another. To achieve this we use the
-        // `tokio::spawn` function to execute the work in the background.
-        //
-        // Essentially here we're executing a new task to run concurrently,
-        // which will allow all of our clients to be processed concurrently.
-
-        tokio::spawn(async move {
-            let mut buf = vec![0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
-
-                if n == 0 {
-                    return;
-                }
-
-                socket
-                    .write_all(&buf[0..n])
-                    .await
-                    .expect("failed to write data to socket");
-            }
-        });
-    }
+async fn main() -> std::io::Result<()> {
+   let listener = TcpListener::bind("127.0.0.1:6379").await?;
+   loop {
+       let (mut socket, _) = listener.accept().await?;
+       tokio::spawn(async move {
+           let mut buf = vec![0; 1024];
+           loop {
+               let n = match socket.read(&mut buf).await {
+                  Ok(n) if n == 0 => return,
+                  Ok(n) => n,
+                  Err(e) => {
+                      eprintln!("failed to read from socket; err = {:?}", e);
+                      return;
+                  }
+               };
+               // Echo the data back to the client
+               if let Err(e) = socket.write_all(&buf[0..n]).await {
+                  eprintln!("failed to write to socket; err = {:?}", e);
+                  return;
+               }
+           }
+       });
+   }
 }
