aa6118da109b01a46958a89ff093f0c866eb511b
Igor <igor@devfire.io>
2023-11-12 14:12:10 -0500

    broken parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,64 +1,38 @@
-// borrowed from https://raw.githubusercontent.com/pawelkobojek/respirator/main/src/parser.rs
 use nom::{
-    bytes::complete::take,
-    character::complete::{crlf, not_line_ending},
-    multi::count,
-    sequence::terminated,
-    IResult,
+    branch::alt,
+    bytes::complete::{tag, take_while},
+    combinator::{map, map_res},
+    sequence::{delimited, preceded, tuple},
+    IResult, character::complete::digit1,
 };
 
-use crate::protocol::RespFrame;
+use crate::protocol::Command;
 
-pub fn parse_resp(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, val) = take(1usize)(input)?;
-    match val[0] {
-        b'+' => simple_string(input),
-        b':' => integer(input),
-        b'-' => error(input),
-        b'$' => bulk_string(input),
-        b'*' => array(input),
-        _ => panic!("Unknown type byte: {:?}", val),
-    }
+fn parse_number(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |digit_str: &str| digit_str.parse::<u32>())(input)
+ }
+ 
+fn parse_bulk_string(input: &str) -> IResult<&str, &str> {
+    preceded(
+        delimited(tag("$"), parse_number, tag("\r\n")),
+        take_while(|c: char| c != '\r'),
+    )(input)
 }
 
-fn simple_string(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, val) = terminated(not_line_ending, crlf)(input)?;
-    Ok((input, RespFrame::SimpleString(val.to_vec())))
+fn parse_echo_command(input: &str) -> IResult<&str, &str> {
+    map(
+        tuple((
+            delimited(tag("*2\r\n"), tag(r"\$4\r\nECHO\r\n"), tag("\r\n")),
+            parse_bulk_string,
+        )),
+        |(_command_name, message)| message,
+    )(input)
 }
 
-fn integer(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, val) = terminated(not_line_ending, crlf)(input)?;
-    Ok((
-        input,
-        RespFrame::Integer(String::from_utf8_lossy(val).parse::<i64>().unwrap()),
-    ))
-}
-
-fn error(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, val) = terminated(not_line_ending, crlf)(input)?;
-    Ok((input, RespFrame::Error(val.to_vec())))
-}
-
-fn bulk_string(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, len) = length(input)?;
-    if len == 0 {
-        return Ok((input, RespFrame::BulkString(None)));
-    }
-    let (input, val) = terminated(take(len), crlf)(input)?;
-
-    Ok((input, RespFrame::BulkString(Some(val.to_vec()))))
-}
-
-fn length(input: &[u8]) -> IResult<&[u8], usize> {
-    let (input, len) = terminated(not_line_ending, crlf)(input)?;
-    Ok((input, String::from_utf8_lossy(len).parse().unwrap()))
-}
-
-fn array(input: &[u8]) -> IResult<&[u8], RespFrame> {
-    let (input, len) = length(input)?;
-    if len == 0 {
-        return Ok((input, RespFrame::Array(None)));
-    }
-    let (input, res) = count(parse_resp, len)(input)?;
-    Ok((input, RespFrame::Array(Some(res))))
+fn parse_command(input: &str) -> IResult<&str, Command> {
+    alt((
+        map(tag(r"*1\r\n\$4\r\nPING\r\n"), |_| Command::Ping),
+        map(parse_echo_command, |msg| Command::Echo(msg.into())),
+        // Map other commands here
+    ))(input)
 }
