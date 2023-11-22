bacbe1d4b7e286446acc24cfde7cc7f1edbc5b92
Igor <igor@devfire.io>
2023-11-22 14:48:10 -0500

    brought back the initial resp basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
-use codec::RespCodec;
+// use anyhow;
+
 use env_logger::Env;
-
-use errors::RedisError;
-use futures_util::StreamExt;
 use log::{info, warn};
-use protocol::RespFrame;
-
+use resp::{Decoder, Value};
+use tokio::io::AsyncReadExt;
 use tokio::net::{TcpListener, TcpStream};
 
-use tokio_util::codec::{FramedRead, FramedWrite};
-
-use crate::handlers::handle_array;
-
-mod codec;
-mod errors;
-mod handlers;
-mod parser;
-mod protocol;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<(), RedisError> {
+async fn main() -> std::io::Result<()> {
     // Setup the logging framework
     let env = Env::default()
         .filter_or("LOG_LEVEL", "info")
@@ -33,43 +22,56 @@ async fn main() -> anyhow::Result<(), RedisError> {
 
     loop {
         // Asynchronously wait for an inbound TcpStream.
-        let (stream, _address) = listener.accept().await?;
+        let (stream, _) = listener.accept().await?;
 
         // Spawn our handler to be run asynchronously.
-        // A new task is spawned for each inbound socket.
-        // The socket is moved to the new task and processed there.
+        // A new task is spawned for each inbound socket.  The socket is
+        // moved to the new task and processed there.
         tokio::spawn(async move {
-            process(stream).await.expect("Failed spawning process()");
+            process(stream).await;
         });
     }
 }
 
-async fn process(stream: TcpStream) -> anyhow::Result<()> {
-    let (reader, writer) = stream.into_split();
+async fn process(stream: TcpStream) {
+    let (mut reader, mut writer) = stream.into_split();
 
-    let mut reader = FramedRead::new(reader, RespCodec::new());
-    let mut writer = FramedWrite::new(writer, RespCodec::new());
+    //let mut reader = BufReader::new(reader);
 
-    // Redis generally uses RESP as a request-response protocol in the following way:
-    //  Clients send commands to a Redis server as an array of bulk strings.
-    //  The first (and sometimes also the second) bulk string in the array is the command's name.
-    //  Subsequent elements of the array are the arguments for the command.
-    // The server replies with a RESP type.
-    while let Some(message) = reader.next().await {
-        match message {
-            Ok(RespFrame::Array(value)) => {
-                if let Some(msg) = value {
-                    info!("Got an array: {:?}", msg);
-                    handle_array(msg, &mut writer).await?;
-                }
+    loop {
+        // Buffer to store the data
+        let mut buf = vec![0; 1024];
+
+        // Read data from the stream, n is the number of bytes read
+        let n = reader
+            .read(&mut buf)
+            .await
+            .expect("Unable to read from buffer");
+
+        if n == 0 {
+            warn!("Error: buffer empty");
+            break;
+        }
+
+        info!("Read {} bytes", n);
+
+        // https://docs.rs/resp/latest/resp/struct.Decoder.html
+        let mut decoder = Decoder::new(std::io::BufReader::new(buf.as_slice()));
+
+        let request: resp::Value = decoder.decode().expect("Unable to decode request");
+        info!("Received {:?}", request);
+
+        match request {
+            Value::Null => todo!(),
+            Value::NullArray => todo!(),
+            Value::String(_) => todo!(),
+            Value::Error(_) => todo!(),
+            Value::Integer(_) => todo!(),
+            Value::Bulk(_) => todo!(),
+            Value::BufBulk(_) => todo!(),
+            Value::Array(array) => {
+                info!("Array received {:?}", array);
             }
-
-            Ok(_) => {
-                warn!("NOTE: RESP requests must be made as an array of bulk strings.");
-            }
-            Err(_) => todo!(),
         }
     }
-
-    Ok(())
 }
