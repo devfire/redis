83c7085765dfb31e577e0bf9d0e619535b74ee4f
Igor <igor@devfire.io>
2023-11-24 14:10:29 -0500

    fixed empty ECHO msg

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ fn handler(value: Value, array: &mut Vec<Value>) -> Option<RedisCommand> {
                     if !array.is_empty() {
                         let command_message = array.remove(0); // 0th element, i.e. first one
 
-                        //https://docs.rs/resp/latest/resp/enum.Value.html
+                        // https://docs.rs/resp/latest/resp/enum.Value.html
                         // Remember, in COMMAND DOCS, DOCS is optional, so it needs a Some().
                         // Then this function returns an Option<> so we need one more Some().
                         return Some(RedisCommand::Command(Some(command_message)));
@@ -75,17 +75,19 @@ fn handler(value: Value, array: &mut Vec<Value>) -> Option<RedisCommand> {
                     //
                     // Then, we are popping off one more element to grab the message that followed ECHO
                     // let's make sure the array is not empty first, ECHO can be by itself with no message
+                    let mut echo_message = Value::String("".to_string());
+
                     if !array.is_empty() {
-                        let echo_message = array.remove(0); // 0th element, i.e. first one
+                        echo_message = array.remove(0); // 0th element, i.e. first one
 
                         info!("Assembling ECHO + {:?}", echo_message);
                         //https://docs.rs/resp/latest/resp/enum.Value.html
                         // Remember, Echo "MESSAGE", message is optional, so it needs a Some().
                         // Then this function returns an Option<> so we need one more Some().
-                        return Some(RedisCommand::Echo(Some(echo_message)));
-                    } else {
-                        None
                     }
+
+                    // Looks like we got ECHO followed by nothing so simply reply with an empty string.
+                    return Some(RedisCommand::Echo(Some(echo_message)));
                 } // _ => None,
             }
         }
