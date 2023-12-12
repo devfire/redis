b48f3f994ed22d7e834046206a5f89a3a8c4e416
Igor Kantor <igor@devfire.io>
2023-12-12 14:00:17 -0500

    initial set command struct seutp

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,14 +1,23 @@
-use resp::Value;
-use strum_macros::{Display, EnumString};
-// pub type SetCommandType = (String, Vec<Value>);
-pub type SetCommandType = (String, Value);
-
-#[derive(Debug, PartialEq, EnumString, Display)]
-#[strum(ascii_case_insensitive)]
+#[derive(Debug)]
 pub enum RedisCommand {
     Ping,
     Echo(String),
     Command,
-    // Set(Option<SetCommandType>), // key, value tuple for the Set command
+    Set(SetCommandParameters), // key, value tuple for the Set command
     // Get(Option<String>),
+}
+
+// SET key value [NX | XX] [GET] [EX seconds | PX milliseconds | EXAT unix-time-seconds | PXAT unix-time-milliseconds | KEEPTTL]
+#[derive(Debug)]
+pub struct SetCommandParameters {
+    pub key: String,
+    pub value: String,
+    pub option: Option<SetCommandSetOption>,
+    pub expire: Option<usize>,
+}
+
+#[derive(Debug)]
+pub enum SetCommandSetOption {
+    NX,
+    XX,
 }
