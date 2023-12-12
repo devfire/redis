f9a3aff5cc801ac2f75107261b1d045e3d6db22e
Igor Kantor <igor@devfire.io>
2023-12-12 17:59:32 -0500

    set works for kv pairs

--- src/parsers.rs
+++ src/parsers.rs
@@ -13,7 +13,10 @@ use nom::{
 };
 use resp::Value;
 
-use crate::{errors::RedisError, protocol::RedisCommand};
+use crate::{
+    errors::RedisError,
+    protocol::{RedisCommand, SetCommandParameters},
+};
 
 /// Goes through the array one element at a time.
 /// If it detects a matching command, attempts to assemble the command with its proper parameters.
@@ -51,6 +54,32 @@ fn parse_echo(input: &str) -> IResult<&str, RedisCommand> {
     Ok((input, RedisCommand::Echo(echo_string.to_string())))
 }
 
+fn parse_set(input: &str) -> IResult<&str, RedisCommand> {
+    // test string: *3\r\n$3\r\nset\r\n$5\r\nhello\r\n$7\r\noranges\r\n
+    let (input, _) = tag("*")(input)?;
+    let (input, _len) = (length)(input)?; // length eats crlf
+    let (input, _) = tag_no_case("$3\r\nSET\r\n$")(input)?;
+
+    // get the key first
+    let (input, _key_length) = (length)(input)?;
+    let (input, key) = terminated(not_line_ending, crlf)(input)?;
+
+    // let's get the value next
+    let (input, _) = tag("$")(input)?;
+    let (input, _value_length) = (length)(input)?;
+    let (input, value) = terminated(not_line_ending, crlf)(input)?;
+
+    // everything from here on is optional
+    // let (input, _) = opt(tag("$"))(input)?;
+    let set_params = SetCommandParameters {
+        key: key.to_string(),
+        value: value.to_string(),
+        option: None,
+        expire: None,
+    };
+    Ok((input, RedisCommand::Set(set_params)))
+}
+
 pub fn parse_command(input: &str) -> IResult<&str, RedisCommand> {
     alt((
         map(tag_no_case("*1\r\n$4\r\nPING\r\n"), |_| RedisCommand::Ping),
@@ -58,5 +87,6 @@ pub fn parse_command(input: &str) -> IResult<&str, RedisCommand> {
             RedisCommand::Command
         }),
         parse_echo,
+        parse_set,
     ))(input)
 }
