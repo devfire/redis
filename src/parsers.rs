a3e29213d511759f45116fe215023eb70ce5f282
Igor Kantor <igor@devfire.io>
2023-12-16 16:01:14 -0500

    minor comments

--- src/parsers.rs
+++ src/parsers.rs
@@ -46,33 +46,31 @@ fn parse_set(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _len) = (length)(input)?; // length eats crlf
     let (input, _) = tag_no_case("$3\r\nSET\r\n")(input)?;
 
-    /*
-    tuple: The tuple combinator is used to apply a tuple of parsers one by one and return their results as a tuple. 
-    In this case, it's used to parse two strings followed by an optional option, a GET flag, and an expiration option
-    
-    opt: The opt combinator is used to make the parsing of the option, GET flag, and expiration option optional. 
-    If these options are not present in the input string, opt will return None.
-    
-    alt: The alt combinator is used to try multiple parsers in order until one succeeds. 
-    In this case, it's used to parse either the "NX" or "XX" option.
-    
-    map: The map combinator is used to transform the output of the parser. 
-    In this case, it's used to map the result of the tag_no_case combinator to true for the GET flag, 
-    and to parse the seconds or milliseconds for the expiration option.
-    
-    tag_no_case: The tag_no_case combinator is used to match a case-insensitive string. 
-    In this case, it's used to match the strings "$2\r\nNX\r\n", "$2\r\nXX\r\n", "$3\r\nGET\r\n", "$2\r\nEX\r\n", and "$2\r\nPX\r\n",
-    each one a potential expiration option in redis SET command.
-    
-    value: The value combinator is used to map the result of a parser to a specific value. 
-    In this case, it's used to map the result of the tag_no_case combinator to SetCommandSetOption::NX or 
-    SetCommandSetOption::XX for the option.
-    
-    parse_resp_string: This parser is used to parse a RESP string.
-    
-    Summary: This parser returns a tuple containing the parsed key, value, option, GET flag, and expiration option. 
-    If the option, GET flag, or expiration option are not present in the input string, they will be None.
-     */
+    // tuple: The tuple combinator is used to apply a tuple of parsers one by one and return their results as a tuple. 
+    // In this case, it's used to parse two strings followed by an optional option, a GET flag, and an expiration option
+    //
+    // opt: The opt combinator is used to make the parsing of the option, GET flag, and expiration option optional. 
+    // If these options are not present in the input string, opt will return None.
+    //
+    // alt: The alt combinator is used to try multiple parsers in order until one succeeds. 
+    // In this case, it's used to parse either the "NX" or "XX" option.
+    //
+    // map: The map combinator is used to transform the output of the parser. 
+    // In this case, it's used to map the result of the tag_no_case combinator to true for the GET flag, 
+    // and to parse the seconds or milliseconds for the expiration option.
+    //
+    // tag_no_case: The tag_no_case combinator is used to match a case-insensitive string. 
+    // In this case, it's used to match the strings "$2\r\nNX\r\n", "$2\r\nXX\r\n", "$3\r\nGET\r\n", "$2\r\nEX\r\n", and "$2\r\nPX\r\n",
+    // each one a potential expiration option in redis SET command.
+    //
+    // value: The value combinator is used to map the result of a parser to a specific value. 
+    // In this case, it's used to map the result of the tag_no_case combinator to SetCommandSetOption::NX or 
+    // SetCommandSetOption::XX for the option.
+    //
+    // parse_resp_string: This parser is used to parse a RESP string.
+    //
+    // Summary: This parser returns a tuple containing the parsed key, value, option, GET flag, and expiration option. 
+    // If the option, GET flag, or expiration option are not present in the input string, they will be None.
     let (input, (key, value, option, get, expire)) = tuple((
         parse_resp_string, // key
         parse_resp_string, // value
