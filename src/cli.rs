33ceabb4c9eba619f006a9512520bdc759d7a973
Igor Kantor <igor@devfire.io>
2024-01-21 14:52:33 -0500

    10 implement command line parsing (#11)
    * initial clap commit
    
    * added default clap values

---
+++ src/cli.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// The directory where RDB files are stored
+    #[arg(long)]
+    #[clap(default_value = "./")]
+    pub dir: Option<String>,
+
+    /// The name of the RDB file
+    #[arg(long, value_name = "FILE")]
+    #[clap(default_value = "config.rdb")]
+    pub dbfilename: Option<PathBuf>,
+}
