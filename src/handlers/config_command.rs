3e2d6b29639bf3b06cc8986fdd17b5075a6f7e2d
Igor Kantor <igor@devfire.io>
2024-01-26 13:02:57 -0500

    config_command_handler first implementation

---
+++ src/handlers/config_command.rs
@@ -0,0 +1,53 @@
+use tokio::sync::{mpsc, oneshot};
+
+use crate::{actors::config::ConfigCommandActor, messages::ConfigActorMessage};
+
+#[derive(Clone)]
+pub struct ConfigCommandActorHandle {
+    sender: mpsc::Sender<ConfigActorMessage>,
+}
+
+// Gives you access to the underlying actor.
+impl ConfigCommandActorHandle {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel(8);
+        let mut actor = ConfigCommandActor::new(receiver);
+        tokio::spawn(async move { actor.run().await });
+
+        Self { sender }
+    }
+
+    /// implements the redis GET command, taking a key as input and returning a value.
+    /// https://redis.io/commands/get/
+    pub async fn get_value(&self, key: &str) -> Option<String> {
+        let (send, recv) = oneshot::channel();
+        let msg = ConfigActorMessage::GetValue {
+            config_key: key.to_string(),
+            respond_to: send,
+        };
+
+        // Ignore send errors. If this send fails, so does the
+        // recv.await below. There's no reason to check the
+        // failure twice.
+        let _ = self.sender.send(msg).await;
+
+        // this is going back once the msg comes back from the actor.
+        // NOTE: we might get None back, i.e. no value for the given key.
+        if let Some(value) = recv.await.expect("Actor task has been killed") {
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    /// implements the redis SET command, taking a key, value pair as input. Returns nothing.
+    pub async fn set_value(&self, config_key: &str, config_value: &str) {
+        let msg = ConfigActorMessage::SetValue {
+            config_key: config_key.to_string(),
+            config_value: config_value.to_string(),
+        };
+
+        // Ignore send errors.
+        let _ = self.sender.send(msg).await.expect("Failed to set value.");
+    }
+}
--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,1 +1,2 @@
 pub(crate) mod set_command;
+pub(crate) mod config_command;
