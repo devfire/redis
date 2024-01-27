1fbfefbc6b63cc075eeeeaf08b07971593d4f1da
Igor Kantor <igor@devfire.io>
2024-01-26 21:15:37 -0500

    minor comment formatting fixes

--- src/parsers.rs
+++ src/parsers.rs
@@ -58,16 +58,18 @@ fn parse_append(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _) = tag("*")(input)?;
     let (input, _len) = (length)(input)?; // length eats crlf
     let (input, _) = tag_no_case("$6\r\nAPPEND\r\n")(input)?;
-    
+
     // let's get the key to append to, first
     let (input, key) = (parse_resp_string)(input)?;
 
     // now let's grab the value we are appending
     let (input, value) = (parse_resp_string)(input)?;
 
-    Ok((input, RedisCommand::Append(key.to_string(), value.to_string())))
+    Ok((
+        input,
+        RedisCommand::Append(key.to_string(), value.to_string()),
+    ))
 }
-
 
 fn parse_del(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _) = tag("*")(input)?;
@@ -75,8 +77,8 @@ fn parse_del(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _) = tag_no_case("$3\r\nDEL\r\n")(input)?;
 
     // many1 runs the embedded parser, gathering the results in a Vec.
-    // This stops on Err::Error if there is at least one result, 
-    // and returns the results that were accumulated. 
+    // This stops on Err::Error if there is at least one result,
+    // and returns the results that were accumulated.
     let (input, keys_to_delete) = nom::multi::many1(parse_resp_string)(input)?;
     Ok((input, RedisCommand::Del(keys_to_delete)))
 }
@@ -87,8 +89,8 @@ fn parse_mget(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _) = tag_no_case("$4\r\nMGET\r\n")(input)?;
 
     // many1 runs the embedded parser, gathering the results in a Vec.
-    // This stops on Err::Error if there is at least one result, 
-    // and returns the results that were accumulated. 
+    // This stops on Err::Error if there is at least one result,
+    // and returns the results that were accumulated.
     let (input, keys_to_get) = nom::multi::many1(parse_resp_string)(input)?;
     Ok((input, RedisCommand::Mget(keys_to_get)))
 }
@@ -99,43 +101,38 @@ fn parse_set(input: &str) -> IResult<&str, RedisCommand> {
     let (input, _len) = (length)(input)?; // length eats crlf
     let (input, _) = tag_no_case("$3\r\nSET\r\n")(input)?;
 
+    // Summary: This parser returns a tuple containing the parsed key, value, option, GET flag, and expiration option.
+    // If the option, GET flag, or expiration option are not present in the input string, they will be None.
     // tuple: The tuple combinator is used to apply a tuple of parsers one by one and return their results as a tuple.
     // In this case, it's used to parse two strings followed by an optional option, a GET flag, and an expiration option
     //
-    // opt: The opt combinator is used to make the parsing of the option, GET flag, and expiration option optional.
-    // If these options are not present in the input string, opt will return None.
-    //
-    // alt: The alt combinator is used to try multiple parsers in order until one succeeds.
-    // In this case, it's used to parse either the "NX" or "XX" option.
-    //
-    // map: The map combinator is used to transform the output of the parser.
-    // In this case, it's used to map the result of the tag_no_case combinator to true for the GET flag,
-    // and to parse the seconds or milliseconds for the expiration option.
-    //
-    // tag_no_case: The tag_no_case combinator is used to match a case-insensitive string.
-    // In this case, it's used to match the strings "$2\r\nNX\r\n", "$2\r\nXX\r\n", "$3\r\nGET\r\n", "$2\r\nEX\r\n", and "$2\r\nPX\r\n",
-    // each one a potential expiration option in redis SET command.
-    //
-    // value: The value combinator is used to map the result of a parser to a specific value.
-    // In this case, it's used to map the result of the tag_no_case combinator to SetCommandSetOption::NX or
-    // SetCommandSetOption::XX for the option.
-    //
-    // parse_resp_string: This parser is used to parse a RESP string.
-    //
-    // Summary: This parser returns a tuple containing the parsed key, value, option, GET flag, and expiration option.
-    // If the option, GET flag, or expiration option are not present in the input string, they will be None.
     let (input, (key, value, option, get, expire)) = tuple((
         parse_resp_string, // key
         parse_resp_string, // value
+        // opt: The opt combinator is used to make the parsing of the option, GET flag, and expiration option optional.
+        // If these options are not present in the input string, opt will return None.
+        // alt: The alt combinator is used to try multiple parsers in order until one succeeds.
+        // In this case, it's used to parse either the "NX" or "XX" option.
         opt(alt((
-            // either NX or XX
+            // value: The value combinator is used to map the result of a parser to a specific value.
+            // In this case, it's used to map the result of the tag_no_case combinator to SetCommandSetOption::NX or
+            // SetCommandSetOption::XX for the option.
+            //
             value(SetCommandSetOption::NX, tag_no_case("$2\r\nNX\r\n")),
             value(SetCommandSetOption::XX, tag_no_case("$2\r\nXX\r\n")),
         ))),
         // GET: Return the old string stored at key, or nil if key did not exist.
+        // tag_no_case: The tag_no_case combinator is used to match a case-insensitive string.
+        // In this case, it's used to match the strings "$2\r\nNX\r\n", "$2\r\nXX\r\n", "$3\r\nGET\r\n", "$2\r\nEX\r\n", and "$2\r\nPX\r\n",
+        // each one a potential expiration option in redis SET command.
+        //
         opt(map(tag_no_case("$3\r\nGET\r\n"), |_| true)),
         // These maps all handle the various expiration options.
         opt(alt((
+            // map: The map combinator is used to transform the output of the parser.
+            // In this case, it's used to map the result of the tag_no_case combinator to true for the GET flag,
+            // and to parse the seconds or milliseconds for the expiration option.
+            //
             map(
                 tuple((tag_no_case("$2\r\nEX\r\n"), parse_resp_string)),
                 |(_expire_option, seconds)| {
@@ -200,6 +197,6 @@ pub fn parse_command(input: &str) -> IResult<&str, RedisCommand> {
         parse_strlen,
         parse_mget,
         parse_append,
-        parse_config
+        parse_config,
     ))(input)
 }
