952444b34608afae799c6c831a23ecf929e35c10
Igor Kantor <igor@devfire.io>
2024-01-27 23:08:27 -0500

    WIP broken code

--- src/actors/mod.rs
+++ src/actors/mod.rs
@@ -1,3 +1,10 @@
+/// Modules for actor implementations and configuration.
+///
+/// The `config` module contains actor configuration code.
+///
+/// The `set` module contains set actor implementations.  
+///
+/// The `process` module contains process actor implementations.
 pub(crate) mod config;
-pub(crate) mod set;
 pub(crate) mod process;
+pub(crate) mod set;
