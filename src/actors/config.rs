fa718eba84e5a16039ee3d85368d5e6d26eadd89
Igor Kantor <igor@devfire.io>
2024-07-15 17:22:31 -0400

    48 rdb transfer (#52)
    * dropped an empty rdb file for testing
    
    * added info to config file load
    
    * added full dir listing
    
    * removed dump
    
    * removed dump.bkup
    
    * added dump.rdb
    
    * added hex representation of the empty rdb file
    
    * switched back to oneshot and a Vec<Vec<u8>> for processor responses

--- src/actors/config.rs
+++ src/actors/config.rs
@@ -18,6 +18,7 @@ use tokio_util::codec::FramedRead;
 use std::{collections::HashMap, path::Path};
 
 // use tokio::net::TcpStream;
+use tokio::fs;
 use tokio::sync::mpsc;
 
 // include the format.rs file from rdb
@@ -153,10 +154,25 @@ impl ConfigCommandActor {
                 respond_to,
             } => {
                 let fullpath = format!("{}/{}", dir, dbfilename);
+                // list all the files in the directory
+
+                let current_dir = Path::new(".");
+                let mut entries = fs::read_dir(current_dir)
+                    .await
+                    .expect("Failed to read directory");
+
+                while let Some(entry) = entries.next_entry().await.expect("Failed to read entry") {
+                    let path = entry.path();
+                    if path.is_file() {
+                        println!("File: {:?}", path);
+                    } else if path.is_dir() {
+                        println!("Directory: {:?}", path);
+                    }
+                }
 
                 // check to see if the file exists.
                 if !Path::new(&fullpath).exists() {
-                    log::error!("Config file does not exist.");
+                    log::error!("Config file {} does not exist.", fullpath);
                     let _ = respond_to.send(None); // this will be turned into an Err in the handler
                 } else {
                     // file exists, let's proceed.
