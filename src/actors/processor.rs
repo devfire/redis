defbb3abac90d2dc601b1c4ee00d23bcb641885b
Igor Kantor <igor@devfire.io>
2024-07-15 10:00:15 -0400

    added hex representation of the empty rdb file

--- src/actors/processor.rs
+++ src/actors/processor.rs
@@ -362,10 +362,14 @@ impl ProcessorActor {
 
                                     let mut reply: Vec<u8> = Vec::new();
 
-                                    let rdb_file_contents = config_command_actor_handle
-                                        .get_config()
-                                        .await
-                                        .expect("Failed to load config file into memory.");
+                                    let rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
+
+                                    let rdb_file_contents = hex::decode(rdb_hex).expect("Failed to decode hex");
+
+                                    // let rdb_file_contents = config_command_actor_handle
+                                    //     .get_config()
+                                    //     .await
+                                    //     .expect("Failed to load config file into memory.");
 
                                     info!("Retrieved config file contents {:?}.", rdb_file_contents);
 
