4ba6d1c8456d689e07400edda749407cd2afc05c
Igor Kantor <igor@devfire.io>
2024-09-10 22:58:08 -0400

    minor tweak

--- src/actors/processor.rs
+++ src/actors/processor.rs
@@ -621,7 +621,7 @@ impl ProcessorActor {
                                 let replconf_getack_star =
                                     RespValue::array_from_slice(&["REPLCONF", "GETACK", "*"]);
 
-                                let _ = replica_tx.send(replconf_getack_star.clone())?;
+                                // let _ = replica_tx.send(replconf_getack_star.clone())?;
 
                                 // get the replica count
                                 let replicas_in_sync =
