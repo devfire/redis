0574255a7902d14cb4083d5e94af686f06884cc6
Igor Kantor <igor@devfire.io>
2024-11-17 16:44:59 -0500

    tracing::instrument added to handle connection

--- src/main.rs
+++ src/main.rs
@@ -285,7 +285,7 @@ async fn main() -> anyhow::Result<()> {
 // But the handle_connection_from_clients() function will only be receiving commands from clients and sending replies.
 // In other words, a redis instance can be both, a replica client to the master, and a server to its own clients.
 // So, this is the "server" part of the redis instance.
-// #[tracing::instrument]
+#[tracing::instrument]
 async fn handle_connection_from_clients(
     stream: TcpStream,
     set_command_actor_handle: SetCommandActorHandle,
